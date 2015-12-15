3c32ce3e68a74294e6fd03ec91c6a053e54945ad
max <max14_1994@ukr.net>
2015-12-15 15:59:23 +0200

    ????????? ???????? ??? Movie ?? ?????? Data Layer - MovieDL.

---
+++ WpfCritic/WpfCritic/DataLayer/MovieDL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCritic.DataLayer
+{
+    public class MovieDL
+    {
+        private DataRow _row;
+
+        public DataRow Row
+        {
+            get { return _row; }
+        }
+
+        public int MovieID { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Company { get; set; }
+        public string Poster { get; set; }
+        public string Name { get; set; }
+        public uint Runtime { get; set; }
+        public string OfficialSite { get; set; }
+        public string Trailer { get; set; }
+
+        public MovieDL(DataRow row)
+        {
+            _row = row;
+        }
+
+        internal void SetRow(DataRow dataRow)
+        {
+            if (_row == null)
+                _row = dataRow;
+        }
+    }
+}
