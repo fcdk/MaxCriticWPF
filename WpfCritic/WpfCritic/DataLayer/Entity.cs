6ccedf8abb328c837ce2e64dc98a4c9ac82e4cdd
max <max14_1994@ukr.net>
2016-01-07 02:02:11 +0200

    ????????? ????? Entity, ???????? Stuff.

--- WpfCritic/WpfCritic/DataLayer/Entity.cs
+++ WpfCritic/WpfCritic/DataLayer/Entity.cs
@@ -21,13 +21,6 @@ namespace WpfCritic.DataLayer
         }
 
         public Guid Id { get; }
-        public DateTime ReleaseDate { get; set; }
-        public string Company { get; set; }
-        public byte[] Poster { get; set; }
-        public string Summary { get; set; }
-        public string Name { get; set; }
-        public string BuyLink { get; set; }
-        public string MainLanguage { get; set; }
 
     }
 }
---
+++ WpfCritic/WpfCritic/DataLayer/Stuff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCritic.DataLayer
+{
+    public class Stuff : Entity<Stuff>
+    {
+        public DateTime ReleaseDate { get; set; }
+        public string Company { get; set; }
+        public byte[] Poster { get; set; }
+        public string Summary { get; set; }
+        public string Name { get; set; }
+        public string BuyLink { get; set; }
+        public string MainLanguage { get; set; }
+    }
+}
